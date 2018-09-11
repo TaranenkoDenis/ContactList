f4754e09642f0525389e9193c4a4216da7296b80
TaranenkoDenis <denis.taranenko@windmill.ch>
2018-09-11 16:35:47 +0300

    Initial commit

---
+++ app/src/main/java/com/example/denis/contactlist/UtilsForTesting.kt
@@ -0,0 +1,6 @@
+package com.example.denis.contactlist
+
+fun sum(first: Int, second: Int) : Int {
+    return first + second
+}
+
---
+++ app/src/test/java/com/example/denis/contactlist/ExampleUnitTest.kt
@@ -0,0 +1,35 @@
+package com.example.denis.contactlist
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun add_isCorrect() {
+        assertEquals(4, sum(2, 2))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun add_twoDigits_isCorrect() {
+        assertEquals(44, sum(22, 22))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun add_big_isCorrect() {
+        assertEquals(4444, sum(2222, 2222))
+    }
+}
